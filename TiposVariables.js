1d7d112bf3b9c77a8a5835af5b902c0e0a546340
Dylan Bolivar <dilanstivel9@gmail.com>
2020-09-01 21:11:50 -0500

    First Code Learning

---
+++ TiposVariables.js
@@ -0,0 +1,11 @@
+// Tipos de Datos
+
+var nombre;
+var cadena = 'Carlos'; // String
+var numero = 12; // number
+var booleano = true; // boolean true or false
+var esNull = null; // null
+var persona = { nombre: 'Dylan', apellido: 'Bolivar', edad: '19' }; // Object
+var array = 
+
+console.log(persona);
